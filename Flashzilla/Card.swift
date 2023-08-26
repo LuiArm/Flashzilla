8cdfe6aa160cc9026fd7fc3312bd3b36b013d65f
LuiArm <Luisarmendariz24@icloud.com>
2023-08-25 22:12:14 -0600

    Creating card struct and view

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by luis armendariz on 8/25/23.
+//
+
+import Foundation
+
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doc in doctor who?", answer: "Jodie Whittaker")
+}
--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: Card.example, count: 12)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        CardView(card: Card.example)
     }
 }
 
